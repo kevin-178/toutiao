37a51ed71db42b7f5468a7cc81e69c99ea2b887d
kevin-178 <2269324563@qq.com>
2023-01-13 13:21:49 +0800

    加入了移动端rem适配,封装了axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载vant核心组件库
+import Vant from 'vant'
+
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
+//引入全局样式
+import '@/style/index.less'
+
+
+
+// 加载动态rem设置
+import 'amfe-flexible'
+
+// 注册使用vant组件库
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
